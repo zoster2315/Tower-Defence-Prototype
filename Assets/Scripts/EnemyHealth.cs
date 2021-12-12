196daa949966d5c366db58ef4eb3c75927cc6fc5
zoster2315 <zoster2315@gmail.com>
2021-12-12 19:17:40 +0400

    1. Стрельба башен по врагам. 2. Хитпойнты у врага. Смерть врага.

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    [SerializeField] int currentHitPoints = 0;
+
+    private void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0)
+            Destroy(gameObject);
+    }
+}
